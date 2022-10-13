a98ea90f96486b764860732ad33aa6f066e4e2da
Elen Magalhães <fromeleen@gmail.com>
2022-10-13 20:36:53 -0300

    criado componente visual para testar componentes

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,52 @@
+import { ReactNode, InputHTMLAttributes } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+export interface TextInputIconProps {
+    children: ReactNode;
+}
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+function TextInputRoot({ children }: TextInputRootProps) {
+  return (
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+      {children}
+    </div>
+  )
+}
+
+
+
+
+
+function TextInputIcon({ children }: TextInputIconProps) {
+  return (
+    <Slot className="w-6 h-6 text-gray-400">
+      {children}
+    </Slot>
+  )
+}
+
+
+function TextInputInput(props: TextInputInputProps) {
+  return (
+    <input 
+      className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      {...props}
+    />
+  )
+}
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+}
+
+TextInputRoot.displayName = 'TextInput.Root'
+TextInputInput.displayName = 'TextInput.Input'
+TextInputIcon.displayName = 'TextInput.Icon'
